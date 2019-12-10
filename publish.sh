8a394f1b2c75fee879ae18256f962c25d749bd49
joncrall <erotemic@gmail.com>
2019-12-10 18:50:04 -0500

    Add tools to make publishing easier

---
+++ publish.sh
@@ -0,0 +1,231 @@
+#!/bin/bash
+__heredoc__='''
+Script to publish a new version of this library on PyPI
+
+Args:
+    # These environment variables must / should be set
+    TWINE_USERNAME : username for pypi
+    TWINE_PASSWORD : password for pypi
+    USE_GPG : defaults to True
+
+Requirements:
+     twine >= 1.13.0
+     gpg2 >= 2.2.4
+     OpenSSL >= 1.1.1c
+
+Notes:
+    # NEW API TO UPLOAD TO PYPI
+    # https://packaging.python.org/tutorials/distributing-packages/
+
+Usage:
+    cd <YOUR REPO>
+
+    # Set your variables or load your secrets
+    export TWINE_USERNAME=<pypi-username>
+    export TWINE_PASSWORD=<pypi-password>
+
+    source $(secret_loader.sh)
+
+    MB_PYTHON_TAG=cp37-cp37m 
+    MB_PYTHON_TAG=cp36-cp36m 
+    MB_PYTHON_TAG=cp35-cp35m 
+    MB_PYTHON_TAG=cp27-cp27mu
+
+    echo "MB_PYTHON_TAG = $MB_PYTHON_TAG"
+    MB_PYTHON_TAG=$MB_PYTHON_TAG ./run_multibuild.sh
+    DEPLOY_BRANCH=master DEPLOY_REMOTE=ibeis MB_PYTHON_TAG=$MB_PYTHON_TAG ./publish.sh yes
+'''
+
+check_variable(){
+    KEY=$1
+    VAL=${!KEY}
+    echo "[DEBUG] CHECK VARIABLE: $KEY=\"$VAL\""
+    if [[ "$VAL" == "" ]]; then
+        echo "[ERROR] UNSET VARIABLE: $KEY=\"$VAL\""
+        exit 1;
+    fi
+}
+
+# Options
+CURRENT_BRANCH=${CURRENT_BRANCH:=$(git branch | grep \* | cut -d ' ' -f2)}
+DEPLOY_BRANCH=${DEPLOY_BRANCH:=release}
+DEPLOY_REMOTE=${DEPLOY_REMOTE:=origin}
+NAME=${NAME:=$(python -c "import setup; print(setup.NAME)")}
+VERSION=$(python -c "import setup; print(setup.VERSION)")
+MB_PYTHON_TAG=${MB_PYTHON_TAG:=$(python -c "import setup; print(setup.MB_PYTHON_TAG)")}
+
+check_variable CURRENT_BRANCH
+check_variable DEPLOY_BRANCH
+check_variable DEPLOY_REMOTE
+check_variable VERSION || exit 1
+
+TAG_AND_UPLOAD=${TAG_AND_UPLOAD:=$1}
+TWINE_USERNAME=${TWINE_USERNAME:=""}
+TWINE_PASSWORD=${TWINE_PASSWORD:=""}
+
+USE_GPG=${USE_GPG:="True"}
+
+if [[ "$(which gpg2)" != "" ]]; then
+    GPG_EXECUTABLE=${GPG_EXECUTABLE:=gpg2}
+else
+    GPG_EXECUTABLE=${GPG_EXECUTABLE:=gpg}
+fi
+
+GPG_KEYID=${GPG_KEYID:=$(git config --local user.signingkey)}
+GPG_KEYID=${GPG_KEYID:=$(git config --global user.signingkey)}
+
+
+echo "
+=== PYPI BUILDING SCRIPT ==
+CURRENT_BRANCH='$CURRENT_BRANCH'
+DEPLOY_BRANCH='$DEPLOY_BRANCH'
+VERSION='$VERSION'
+TWINE_USERNAME='$TWINE_USERNAME'
+GPG_KEYID = '$GPG_KEYID'
+MB_PYTHON_TAG = '$MB_PYTHON_TAG'
+"
+
+
+echo "
+=== <BUILD WHEEL> ===
+"
+
+
+
+echo "LIVE BUILDING"
+# Build wheel and source distribution
+
+MODE=${MODE:=all}
+
+WHEEL_PATHS=()
+
+if [[ "$MODE" == "sdist" ]]; then
+    python setup.py sdist 
+    WHEEL_PATH=$(ls dist/$NAME-$VERSION*.tar.gz)
+    WHEEL_PATHS+=($WHEEL_PATH)
+elif [[ "$MODE" == "universal" ]]; then
+    python setup.py bdist_wheel --universal
+    WHEEL_PATH=$(ls dist/*-$VERSION-$MB_PYTHON_TAG*.whl)
+    WHEEL_PATHS+=($WHEEL_PATH)
+elif [[ "$MODE" == "bdist" ]]; then
+    echo "Assume wheel has already been built"
+    WHEEL_PATH=$(ls wheelhouse/*-$VERSION-$MB_PYTHON_TAG*.whl)
+    WHEEL_PATHS+=($WHEEL_PATH)
+elif [[ "$MODE" == "all" ]]; then
+    python setup.py sdist 
+    WHEEL_PATH=$(ls dist/$NAME-$VERSION*.tar.gz)
+    WHEEL_PATHS+=($WHEEL_PATH)
+
+    python setup.py bdist_wheel --universal
+    WHEEL_PATH=$(ls dist/*-$VERSION-$MB_PYTHON_TAG*.whl)
+    WHEEL_PATHS+=($WHEEL_PATH)
+
+    WHEEL_PATH=$(ls wheelhouse/*-$VERSION-$MB_PYTHON_TAG*.whl)
+    WHEEL_PATHS+=($WHEEL_PATH)
+else
+    echo "bad mode"
+    exit 1
+fi
+
+echo "
+MODE=$MODE
+VERSION='$VERSION'
+WHEEL_PATHS='$WHEEL_PATHS'
+"
+
+echo "
+=== <END BUILD WHEEL> ===
+"
+
+echo "
+=== <GPG SIGN> ===
+"
+
+
+for WHEEL_PATH in "${WHEEL_PATHS[@]}"
+do
+    echo "WHEEL_PATH = $WHEEL_PATH"
+    check_variable WHEEL_PATH
+    if [ "$USE_GPG" == "True" ]; then
+        # https://stackoverflow.com/questions/45188811/how-to-gpg-sign-a-file-that-is-built-by-travis-ci
+        # secure gpg --export-secret-keys > all.gpg
+
+        # REQUIRES GPG >= 2.2
+        check_variable GPG_EXECUTABLE
+        check_variable GPG_KEYID
+
+        echo "Signing wheels"
+        GPG_SIGN_CMD="$GPG_EXECUTABLE --batch --yes --detach-sign --armor --local-user $GPG_KEYID"
+        ls wheelhouse
+        echo "GPG_SIGN_CMD = $GPG_SIGN_CMD"
+        $GPG_SIGN_CMD --output $WHEEL_PATH.asc $WHEEL_PATH
+
+        echo "Checking wheels"
+        twine check $WHEEL_PATH.asc $WHEEL_PATH
+
+        echo "Verifying wheels"
+        $GPG_EXECUTABLE --verify $WHEEL_PATH.asc $WHEEL_PATH 
+    else
+        echo "USE_GPG=False, Skipping GPG sign"
+    fi
+done
+echo "
+=== <END GPG SIGN> ===
+"
+
+if [[ "$CURRENT_BRANCH" != "$DEPLOY_BRANCH" ]]; then
+    TAG_AND_UPLOAD="no"
+    echo "current branch is not the deploy branch. Forcing tag_and_upload=no"
+fi
+
+
+# Verify that we want to publish
+if [[ "$TAG_AND_UPLOAD" != "yes" ]]; then
+    if [[ "$TAG_AND_UPLOAD" != "no" ]]; then
+        read -p "Are you ready to publish version='$VERSION' on branch='$CURRENT_BRANCH'? (input 'yes' to confirm)" ANS
+        echo "ANS = $ANS"
+        TAG_AND_UPLOAD="$ANS"
+    else
+        echo "WRONG BRANCH: Not ready to publish VERSION='$VERSION' on branch='$CURRENT_BRANCH'" 
+    fi
+else
+    echo "Do not want to publish VERSION='$VERSION' on branch='$CURRENT_BRANCH'" 
+fi
+
+
+if [[ "$TAG_AND_UPLOAD" == "yes" ]]; then
+    check_variable TWINE_USERNAME
+    check_variable TWINE_PASSWORD
+
+    #git tag $VERSION -m "tarball tag $VERSION"
+    #git push --tags $DEPLOY_REMOTE $DEPLOY_BRANCH
+
+    for WHEEL_PATH in "${WHEEL_PATHS[@]}"
+    do
+        if [ "$USE_GPG" == "True" ]; then
+            twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD --sign $WHEEL_PATH.asc $WHEEL_PATH
+        else
+            twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD $WHEEL_PATH 
+        fi
+    done
+    echo """
+        !!! FINISH: LIVE RUN !!!
+    """
+else
+    ls wheelhouse
+    echo """
+        DRY RUN ... Skiping tag and upload
+
+        VERSION = '$VERSION'
+        DEPLOY_REMOTE = '$DEPLOY_REMOTE'
+        CURRENT_BRANCH = '$CURRENT_BRANCH'
+        DEPLOY_BRANCH = '$DEPLOY_BRANCH'
+        TAG_AND_UPLOAD = '$TAG_AND_UPLOAD'
+        WHEEL_PATH = '$WHEEL_PATH'
+        WHEEL_PATHS = '$WHEEL_PATHS'
+
+        To do live run set TAG_AND_UPLOAD=yes and ensure deploy and current branch are the same
+
+        !!! FINISH: DRY RUN !!!
+    """
+fi
--- setup.py
+++ setup.py
@@ -1,13 +1,156 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from os.path import exists
 import os
 import sys
-
-# Monkeypatch distutils.
-import setuptools
-
+import setuptools  # NOQA
 import distutils.errors
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from distutils.log import warn
+
+
+def parse_version(fpath):
+    """
+    Statically parse the version number from a python file
+    """
+    import ast
+    if not exists(fpath):
+        raise ValueError('fpath={!r} does not exist'.format(fpath))
+    with open(fpath, 'r') as file_:
+        sourcecode = file_.read()
+    pt = ast.parse(sourcecode)
+    class VersionVisitor(ast.NodeVisitor):
+        def visit_Assign(self, node):
+            for target in node.targets:
+                if getattr(target, 'id', None) == '__version__':
+                    self.version = node.value.s
+    visitor = VersionVisitor()
+    visitor.visit(pt)
+    return visitor.version
+
+
+def parse_description():
+    """
+    Parse the description in the README file
+
+    CommandLine:
+        pandoc --from=markdown --to=rst --output=README.rst README.md
+        python -c "import setup; print(setup.parse_description())"
+    """
+    from os.path import dirname, join, exists
+    readme_fpath = join(dirname(__file__), 'README.rst')
+    # This breaks on pip install, so check that it exists.
+    if exists(readme_fpath):
+        with open(readme_fpath, 'r') as f:
+            text = f.read()
+        return text
+    return ''
+
+
+def parse_requirements(fname='requirements.txt', with_version=False):
+    """
+    Parse the package dependencies listed in a requirements file but strips
+    specific versioning information.
+
+    Args:
+        fname (str): path to requirements file
+        with_version (bool, default=False): if true include version specs
+
+    Returns:
+        List[str]: list of requirements items
+
+    CommandLine:
+        python -c "import setup; print(setup.parse_requirements())"
+        python -c "import setup; print(chr(10).join(setup.parse_requirements(with_version=True)))"
+    """
+    from os.path import exists
+    import re
+    require_fpath = fname
+
+    def parse_line(line):
+        """
+        Parse information from a line in a requirements text file
+        """
+        if line.startswith('-r '):
+            # Allow specifying requirements in other files
+            target = line.split(' ')[1]
+            for info in parse_require_file(target):
+                yield info
+        else:
+            info = {'line': line}
+            if line.startswith('-e '):
+                info['package'] = line.split('#egg=')[1]
+            else:
+                # Remove versioning from the package
+                pat = '(' + '|'.join(['>=', '==', '>']) + ')'
+                parts = re.split(pat, line, maxsplit=1)
+                parts = [p.strip() for p in parts]
+
+                info['package'] = parts[0]
+                if len(parts) > 1:
+                    op, rest = parts[1:]
+                    if ';' in rest:
+                        # Handle platform specific dependencies
+                        # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
+                        version, platform_deps = map(str.strip, rest.split(';'))
+                        info['platform_deps'] = platform_deps
+                    else:
+                        version = rest  # NOQA
+                    info['version'] = (op, version)
+            yield info
+
+    def parse_require_file(fpath):
+        with open(fpath, 'r') as f:
+            for line in f.readlines():
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    for info in parse_line(line):
+                        yield info
+
+    def gen_packages_items():
+        if exists(require_fpath):
+            for info in parse_require_file(require_fpath):
+                parts = [info['package']]
+                if with_version and 'version' in info:
+                    parts.extend(info['version'])
+                if not sys.version.startswith('3.4'):
+                    # apparently package_deps are broken in 3.4
+                    platform_deps = info.get('platform_deps')
+                    if platform_deps is not None:
+                        parts.append(';' + platform_deps)
+                item = ''.join(parts)
+                yield item
+
+    packages = list(gen_packages_items())
+    return packages
+
+
+def native_mb_python_tag():
+    import sys
+    import platform
+    major = sys.version_info[0]
+    minor = sys.version_info[1]
+    ver = '{}{}'.format(major, minor)
+    if platform.python_implementation() == 'CPython':
+        # TODO: get if cp27m or cp27mu
+        impl = 'cp'
+        if ver == '27':
+            IS_27_BUILT_WITH_UNICODE = True  # how to determine this?
+            if IS_27_BUILT_WITH_UNICODE:
+                abi = 'mu'
+            else:
+                abi = 'm'
+        else:
+            abi = 'm'
+    else:
+        raise NotImplementedError(impl)
+    mb_tag = '{impl}{ver}-{impl}{ver}{abi}'.format(**locals())
+    return mb_tag
+
+
+# Monkeypatch distutils.
 
 try:
     from Cython.Distutils import build_ext
@@ -34,52 +177,58 @@ profile Python applications and scripts either with line_profiler or with the
 function-level profiling tools in the Python standard library.
 """
 
+VERSION = parse_version('line_profiler.py')
+NAME = 'line_profiler'
+
 
 py_modules = ['line_profiler', 'kernprof']
 if sys.version_info > (3, 4):
     py_modules += ['line_profiler_py35']
 
-setup(
-    name = 'line_profiler',
-    version = '2.1.1',
-    author = 'Robert Kern',
-    author_email = 'robert.kern@enthought.com',
-    description = 'Line-by-line profiler.',
-    long_description = long_description,
-    url = 'https://github.com/rkern/line_profiler',
-    download_url = 'https://github.com/rkern/line_profiler/tarball/2.1',
-    ext_modules = [
-        Extension('_line_profiler',
-                  sources=[line_profiler_source, 'timers.c', 'unset_trace.c'],
-                  depends=['python25.pxd'],
-        ),
-    ],
-    license = "BSD",
-    keywords = ['timing', 'timer', 'profiling', 'profiler', 'line_profiler'],
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: C",
-        "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        "Topic :: Software Development",
-    ],
-    py_modules = py_modules,
-    entry_points = {
-        'console_scripts': [
-            'kernprof=kernprof:main',
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        version=VERSION,
+        author='Robert Kern',
+        author_email='robert.kern@enthought.com',
+        description='Line-by-line profiler.',
+        long_description=long_description,
+        url='https://github.com/pyutils/line_profiler',
+        ext_modules=[
+            Extension('_line_profiler',
+                      sources=[line_profiler_source, 'timers.c', 'unset_trace.c'],
+                      depends=['python25.pxd']),
         ],
-    },
-    install_requires = [
-        'IPython>=0.13',
-    ],
-    cmdclass = cmdclass,
-)
+        license="BSD",
+        keywords=['timing', 'timer', 'profiling', 'profiler', 'line_profiler'],
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: C",
+            "Programming Language :: Python",
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: Implementation :: CPython',
+            "Topic :: Software Development",
+        ],
+        py_modules=py_modules,
+        entry_points={
+            'console_scripts': [
+                'kernprof=kernprof:main',
+            ],
+        },
+        install_requires=parse_requirements('requirements/runtime.txt'),
+        extras_require={
+            'all': parse_requirements('requirements.txt'),
+            'tests': parse_requirements('requirements/tests.txt'),
+            'optional': parse_requirements('requirements/optional.txt'),
+        },
+        cmdclass=cmdclass,
+    )
